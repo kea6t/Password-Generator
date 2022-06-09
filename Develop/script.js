05986959db107cc215aa9f5fa2e7e3470bab7044
kea6t <keasay26@gmail.com>
2022-06-09 13:59:07 -0400

    added more comments and making more progress

--- Develop/script.js
+++ Develop/script.js
@@ -16,10 +16,7 @@ var numArr = acceptance.numerical.split('');
 // Variable declarations
 var lengthPass = "";
 var passwordChar = "";
-var upperCaseLetter = "";
-var lowerCaseLetter = "";
-var numChar = "";
-var specialChars = "";
+
 
 console.log(acceptance.lettersLowercase);
 
@@ -30,7 +27,7 @@ var generatePassword = function () {
     // ask the user for password length
     lengthPass = window.prompt("How many characters would you like for your password? 'Must be 8 characters minimum and maximum of 60 characters.'");
     if (lengthPass <= 7 || lengthPass >= 60) {
-      window.alert("You need to provide a valid answer! Please try again.")
+      window.alert("You need to provide a valid answer! Please try again.");
     }
     if (isNaN(lengthPass)) {
       window.alert("Please enter a valid number! Try again.")
@@ -43,45 +40,47 @@ var generatePassword = function () {
   while (passCriteria !== true) {
     window.alert("You must have at least one from the following criteria: Lower case, Upper Case, Numerical case, and Special Character.");
 
-    // upper case confirmation message
+    // ask the user if they want special characters for their password.
     passCriteria.upperCaseLetter = window.confirm("Click OK if you want to add upper case characters to your password");
     // if they click ok
     if (passCriteria.upperCaseLetter === true) {
       // this will add upper case array to passwordChar array 
-      passCriteria.passwordChar += acceptance.lettersUpperCase;
-      window.alert("Nice! Your password will have uppercase characters.")
+      passwordChar += acceptance.lettersUpperCase;
+      window.alert("Nice! Your password will have uppercase characters.");
     } else {
       // alerts the user of no upper case character chosen
       window.alert("Fine! You can chose whatever characters you want.");
     }
 
-    // lower case confirmation message
+    // ask the user if they want lower case characters for their password.
     passCriteria.lowerCaseLetter = window.confirm("Click OK if you want to add lower case character in your password. ");
     // if they click OK
     if (passCriteria.lowerCaseLetter === true) {
       // this will add lower case array to passwordChar array
-      passCriteria.passwordChar += alphabetLowerArr;
-      window.alert("Nice! Your password will have lowercase characters.")
+      passwordChar += alphabetLowerArr;
+      window.alert("Nice! Your password will have lowercase characters.");
     } else {
       // alerts the user of no lower case character chosen
       window.alert("Fine! You can chose whatever characters you want.");
     }
-
+    // ask the user if they want number characters for their password.
     passCriteria.numChar = window.confirm("Click OK if you want to add number character in your password. ");
     // if they click OK 
     if (passCriteria.numChar === true) {
       // this will add numArr to passwordChar array
-      passCriteria.passwordChar += numArr;
-      window.alert("Nice! Your password will have number characters.")
+      passwordChar += numArr;
+      window.alert("Nice! Your password will have number characters.");
     } else {
       // alerts the user of no lower case character chosen
       window.alert("Fine! You can chose whatever characters you want.");
     }
-
+    // ask the user if they want special characters for their password.
     passCriteria.specialChars = window.confirm("Click OK if you want to add special character in your password. ");
+    // if they click OK 
     if(passCriteria.specialChars === true) {
-      passCriteria.passwordChar += acceptance.specialCharacters;
-      window.alert("Nice! Your password will have special characters.")
+      // this will add numArr to passwordChar array
+      passwordChar += acceptance.specialCharacters;
+      window.alert("Nice! Your password will have special characters.");
     } else {
       // alerts the user of no lower case character chosen
       window.alert("Fine! You can chose whatever characters you want.");
@@ -90,27 +89,26 @@ var generatePassword = function () {
     //   window.alert("Please select a character to select from the list of characters");
     // } else {
       break;
-    //}
-    console.log(passCriteria.passwordChar);
-    break;
   }
-  var randPass = "";
-  for (var i = 0; i < lengthPass.length; i++) {
-    randPass += passCriteria.passwordChar[Math.floor(Math.random() * passCriteria.passwordChar.length)];
-    console.log(randPass); 
+  var result = "";
+  for (var i = 0; i < lengthPass; i++) {
+    var randPass= passwordChar[Math.floor(Math.random() * passwordChar.length)];
+    result += passwordChar.charAt(randPass);
+    console.log(result);
+    console.log(result.length);
   }
-  return randPass; 
+  return result; 
 }
 
 var passCriteria = [
   {
-    passwordChar,
-    upperCaseLetter,
-    lowerCaseLetter,
-    numChar,
-    specialChars
+    upperCaseLetter: "",
+    lowerCaseLetter: "",
+    numChar: "",
+    specialChars: ""
   }
 ];
+
 
 // function to collect accepted criteria for password
 function writePassword() {
